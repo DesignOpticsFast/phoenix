6a94bd511b512b52b8c3a45fd4735363445ff219
Psychopasta <markgnicholson@gmail.com>
2025-10-04 12:57:39 -0700

    Sprint 1 Changes

---
+++ build/_bedrock_build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/mark/Projects/bedrock
+# Build directory: /Users/mark/Projects/phoenix/build/_bedrock_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("core")
+subdirs("tests")
