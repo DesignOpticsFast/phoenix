8a2a61a1ebdda1cc19a9c02e4efb362978ee1d11
Mark Nicholson <mark@designopticsfast.com>
2025-10-22 20:14:56 +0000

    fix(macos): handle SVG to PNG conversion in icon generation script
    - First convert SVG to high-res PNG using sips -s format png
    - Then generate all icon sizes from the PNG
    - Clean up temporary files
    - This fixes the 'Can't write format: public.svg-image' error

--- scripts/generate_macos_icon.sh
+++ scripts/generate_macos_icon.sh
@@ -27,16 +27,24 @@ mkdir -p "$ICONSET_DIR"
 # Generate all required icon sizes
 echo "Generating icon sizes..."
 
-sips -z 16   16   "$SVG_FILE" --out "$ICONSET_DIR/icon_16x16.png"
-sips -z 32   32   "$SVG_FILE" --out "$ICONSET_DIR/icon_16x16@2x.png"
-sips -z 32   32   "$SVG_FILE" --out "$ICONSET_DIR/icon_32x32.png"
-sips -z 64   64   "$SVG_FILE" --out "$ICONSET_DIR/icon_32x32@2x.png"
-sips -z 128  128  "$SVG_FILE" --out "$ICONSET_DIR/icon_128x128.png"
-sips -z 256  256  "$SVG_FILE" --out "$ICONSET_DIR/icon_128x128@2x.png"
-sips -z 256  256  "$SVG_FILE" --out "$ICONSET_DIR/icon_256x256.png"
-sips -z 512  512  "$SVG_FILE" --out "$ICONSET_DIR/icon_256x256@2x.png"
-sips -z 512  512  "$SVG_FILE" --out "$ICONSET_DIR/icon_512x512.png"
-sips -z 1024 1024 "$SVG_FILE" --out "$ICONSET_DIR/icon_512x512@2x.png"
+# First convert SVG to a high-res PNG, then use sips to resize
+echo "Converting SVG to high-res PNG..."
+sips -s format png "$SVG_FILE" --out "$ICONSET_DIR/temp_1024.png" -z 1024 1024
+
+# Now generate all sizes from the high-res PNG
+sips -z 16   16   "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_16x16.png"
+sips -z 32   32   "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_16x16@2x.png"
+sips -z 32   32   "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_32x32.png"
+sips -z 64   64   "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_32x32@2x.png"
+sips -z 128  128  "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_128x128.png"
+sips -z 256  256  "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_128x128@2x.png"
+sips -z 256  256  "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_256x256.png"
+sips -z 512  512  "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_256x256@2x.png"
+sips -z 512  512  "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_512x512.png"
+sips -z 1024 1024 "$ICONSET_DIR/temp_1024.png" --out "$ICONSET_DIR/icon_512x512@2x.png"
+
+# Clean up temp file
+rm -f "$ICONSET_DIR/temp_1024.png"
 
 echo "Generated icon sizes:"
 ls -la "$ICONSET_DIR/"
